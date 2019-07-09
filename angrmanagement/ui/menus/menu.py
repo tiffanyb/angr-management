a8bf7f5c0a7ba0a93f2d575a2752e9b1cb2b96c5
Tiffany Bao <tiffany.yzh@gmail.com>
2019-07-10 04:59:38 +0800

    Revert "filter out MenuSeparator (#102)"
    This reverts commit bfb9c36a1021e32b9c68d64e076bf72aaba40f62.

--- angrmanagement/ui/menus/menu.py
+++ angrmanagement/ui/menus/menu.py
@@ -54,8 +54,7 @@ class Menu:
 
     def action_by_key(self, key):
         if not self._keyed_entries:
-            self._keyed_entries = dict((ent.key, ent) for ent in
-                    self.entries if isinstance(ent, MenuEntry)
+            self._keyed_entries = dict((ent.key, ent) for ent in self.entries)
         return self._keyed_entries.get(key, None)
 
     def qmenu(self):
